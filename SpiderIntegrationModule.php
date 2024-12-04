0597b7e07898cf376fa4ad31b996367b28684526
Alexander Ivanets <alex.ivanets@proton.me>
2024-12-04 13:41:10 +0100

    Init commit

---
+++ SpiderIntegrationModule.php
@@ -0,0 +1,11 @@
+<?php
+namespace MyOrg\MyModule;
+
+use ExternalModules\AbstractExternalModule;
+
+class ExternalModule extends AbstractExternalModule {
+    public function redcap_every_page_before_render($project_id) {
+        // Your module logic here
+    }
+}
+>
